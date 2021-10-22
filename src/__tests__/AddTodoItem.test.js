110f6a41d3d46890d5a27b7c11052d34701f6072
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-10-22 11:23:33 +0200

    Edit style of add new to do textarea

--- src/__tests__/AddTodoItem.test.js
+++ src/__tests__/AddTodoItem.test.js
@@ -1,7 +1,16 @@
 import { screen, render } from '@testing-library/react';
 import App from '../App';
-test('Must display: Textarea to enter new todo item', () => {
-  render(<App />);
 
-  expect(screen.queryByTestId('add-new-todo')).toBeVisible();
+describe('Add new todo Item:', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('Must display: Textarea to enter new todo item', () => {
+    expect(screen.queryByTestId('input-new-todo')).toBeVisible();
+  });
+
+  test('Must display: Button to submit the new todo', () => {
+    expect(screen.queryByTestId('submit-new-todo')).toBeVisible();
+  });
 });
--- src/todo/AddNewTodo.js
+++ src/todo/AddNewTodo.js
@@ -3,7 +3,11 @@ import styles from './AddNewTodo.module.css';
 const AddNewTodo = () => {
   return (
     <div className={styles.addNewTodoArea}>
-      <textarea type='text' data-testid='add-new-todo' />
+      <textarea
+        type='text'
+        data-testid='input-new-todo'
+        placeholder='Task To do...'
+      />
     </div>
   );
 };
